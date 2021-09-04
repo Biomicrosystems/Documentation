19327720501af79eed95ed2dd0b3b53de1198244
velajua961 <77356882+velajua961@users.noreply.github.com>
2021-09-04 12:49:07 -0500

    Add files via upload

---
+++ DIY Book/Image Processing Software/Codigo fuente/Mesure_microscope/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mesure_microscope
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void measureWindow1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (Form2 about = new Form2())
+            {
+                about.ShowDialog(this);
+            }
+        }
+
+        private void measureWindow1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

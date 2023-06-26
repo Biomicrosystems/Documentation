873a750d25344f3b20bb29840f5595c0b64fec75
Maidy Cabrera <m.cabrerap@uniandes.edu.co>
2023-06-25 21:40:15 -0500

    Feature: Adjust page

--- Principal-page-biomicrosystems/angular-proyect-principal-page/src/app/app-routing.module.ts
+++ Principal-page-biomicrosystems/angular-proyect-principal-page/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalPageComponent } from './general-pages/pages/principal-page/principal-page.component';
-import { LoginComponent } from './general-pages/pages/login/login.component';
 import { ResearchAndProjectsComponent } from './general-pages/pages/research-and-projects/research-and-projects.component';
 import { MembersComponent } from './general-pages/pages/members/members.component';
 import { ContactComponent } from './general-pages/pages/contact/contact.component';
@@ -11,10 +10,6 @@ const routes: Routes = [
   {
     path: '',
     component: PrincipalPageComponent,
-  },
-  {
-    path: 'biomicrosystem/login',
-    component: LoginComponent,
   },
   {
     path: 'home',
